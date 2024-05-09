15a0d4b3ee8553814dda707e10c6f6af5e231935
Nan Ouk <38890764+oukNan@users.noreply.github.com>
2024-05-09 09:39:16 -0700

    Update script.js
    Updated the API, pull, will need to update HTML component

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText="Wind speed: " + speed + "km/hr";
         document.querySelector(".weather").classList.remove("loading");
+        document.querySelector(".listWeather").innerText = "The DESCRIPTION ";
  },
         search: function (){
             this.fetchWeather(document.querySelector(".searchbar").value);
