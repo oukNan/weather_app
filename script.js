8bddf603eac11a1b3aa3ca9fe9df2805ecc035c3
Nan Ouk <38890764+oukNan@users.noreply.github.com>
2024-05-09 09:50:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ let weather = {
         document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
         document.querySelector(".wind").innerText="Wind speed: " + speed + "km/hr";
         document.querySelector(".weather").classList.remove("loading");
-        document.querySelector(".listWeather").innerText = "The DESCRIPTION " + description;
  },
         search: function (){
             this.fetchWeather(document.querySelector(".searchbar").value);
