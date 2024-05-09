7114ef6f553b45efda1c2b8015b8eaa09044464a
Nan Ouk <38890764+oukNan@users.noreply.github.com>
2024-05-09 13:25:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,30 +3,39 @@ let weather = {
     fetchWeather: function(city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey
         )
-        fetch("https://api.openweathermap.org/data/2.5/forecast/daily?lat=44.34&lon=10.99&cnt=7&appid=" {this.apiKey})
+        fetch("https://api.openweathermap.org/data/2.5/forecast/daily?q=London&units=metric&cnt=7&appid={this.apiKey}")
+        fetch("https://api.openweathermap.org/data/2.5/forecast/daily?lat={lat}&lon={lon}&cnt={cnt}&appid={this.apiKey}" + city)
         .then((response) => response.json()) //the promise to return data
         .then((data) => this.displayWeather(data)); //log data to console for user to see
 
     },
-    displayWeather: function(data, weather) { //assign particular pieces from object
+
+
+
+    displayWeather: function(data) { //assign particular pieces from object
         const { name } = data;
         const{ icon, description } = data.weather[0];
         const {temp, humidity } = data.main;
         const { speed } = data.wind;
-        const { city } = data.city;
-        console.log(name, icon, description, temp, humidity, speed, city);
-        
-        var d = document.getElementByClassName("temp");
-        var n = parseFloat(d);
+        const {city } = city.coord.lat;
+
+        console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Todays weather in: " + name;
         document.querySelector(".icon").src="http://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText= description;
-        document.querySelector(".temp").innerText= n*9 / 5+32.5;
-        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".temp").innerText= temp*9 / 5+32.5;
+        //document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".humidity").innerText = "Daily forecast " + weather;
         document.querySelector(".wind").innerText="Wind speed: " + speed + "km/hr";
-        document.querySelector(".detailsOfInfo").innerText= "Details: " + city;
         document.querySelector(".weather").classList.remove("loading");
  },
+    displayWeather: function(data) {
+        const { city } = data;
+        const {citydetail } = data.id;
+
+        console.log(city, citydetail);
+        document.querySelector(".detailsOfInfo").innerText = citydetail;
+    },
         search: function (){
             this.fetchWeather(document.querySelector(".searchbar").value);
         },
