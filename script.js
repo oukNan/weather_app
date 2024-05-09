8936dce63e3616bbc013c9dae12764e57797880e
Nan Ouk <38890764+oukNan@users.noreply.github.com>
2024-05-09 08:55:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let weather = {
         const { speed } = data.wind;
 
         console.log(name, icon, description, temp, humidity, speed);
-        document.querySelector(".city").innerText = "Weather in: " + name;
+        document.querySelector(".city").innerText = "Todays weather in: " + name;
         document.querySelector(".icon").src="http://openweathermap.org/img/wn/" + icon + ".png";
         document.querySelector(".description").innerText= description;
         document.querySelector(".temp").innerText= temp * 9 / (5+32.5);
