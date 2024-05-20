fb79668e80fdbcca637a272eff253736da57682c
Nan Ouk <38890764+oukNan@users.noreply.github.com>
2024-05-20 15:53:15 -0700

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let weather = {
         const{ icon, description } = data.weather[0];
         const {temp, humidity } = data.main;
         const { speed } = data.wind;
-        const {city } = city.coord.lat;
+        //const {city } = city.coord.lat;
         
         console.log(name, icon, description, temp, humidity, speed);
         document.querySelector(".city").innerText = "Todays weather in: " + name;
